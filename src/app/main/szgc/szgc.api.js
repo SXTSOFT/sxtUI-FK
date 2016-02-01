69694886ee251687922c4188cf30ec8aaeb4ba88
guowei007 <guowei07@foxmail.com>
2016-02-01 15:05:30 +0800

    项目班组总览表   引用了echars

--- src/app/main/szgc/szgc.api.js
+++ src/app/main/szgc/szgc.api.js
@@ -40,7 +40,21 @@
         //统计工序填报情况(监理)
         GetBatchCount:function(args){
           return $http.get($http.url('/api/Report/GetBatchCount' , args));
+        },
+        // 3.统计项目的合格工序，不合格工序情况(项目总览)
+        GetCountBatchByProject:function (args){
+          return $http.get($http.url('/api/Report/GetCountBatchByProject' , args));
+        },
+        //1.统计班组工序情况(班组)
+        GetCountBatchByGroup:function(args){
+          return $http.get($http.url('/api/Report/GetCountBatchByGroup' , args));
+        },
+        // 2.统计验收批情况表(工序总览)
+        GetBatchDetails:function(args) {
+          return $http.get($http.url('/api/Report/GetBatchDetails' , args));
         }
+
+
       }
     })
   }
