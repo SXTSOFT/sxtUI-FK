afcf48c89242bf9182a9ee2f5283dc85aa28ea28
zhangzhaoyong <jiuyuong@gmail.com>
2016-02-16 15:36:58 +0800

    添加sxtMaps

---
+++ src/app/main/szgc/directives/sxtImageView.js
@@ -0,0 +1,34 @@
+/**
+ * Created by zhangzhaoyong on 16/2/16.
+ */
+(function(){
+  'use strict';
+
+  angular
+    .module('app.szgc')
+    .directive('sxtImageView',sxtImageView);
+
+  /** @ngInject */
+  function sxtImageView(){
+    return {
+      scope:{
+        api:'='
+      },
+      link:link
+    }
+
+    function link(scope,element,attr,ctrl){
+      var preview ;
+      element.click(function(){
+
+        //scope.api().then(function(result){
+        //  // result = [{thumbUrl:'',Url:''}]
+        //   preview = $(element).imagePreview();
+        //})
+      });
+      scope.$on('destroy',function(){
+        preview.destroy();
+      });
+    }
+  }
+})
