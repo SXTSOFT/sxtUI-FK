70f366898cb02aeaa3a6575565dca2509b648d99
zhangzhaoyong <jiuyuong@gmail.com>
2016-03-01 16:34:37 +0800

    更新桌面3级图标

--- src/app/main/szgc/directives/sxtMaps.js
+++ src/app/main/szgc/directives/sxtMaps.js
@@ -33,6 +33,7 @@
           });
 
         layer.addTo(map);
+        var mks = [];
         api.szgc.ProjectExService.query(4).then(function (result) {
           scope.markers = [];
 
@@ -49,12 +50,57 @@
           });
 
           angular.forEach(scope.markers, function (o, k) {
-            L
-              .marker([o.lat, o.lng], o)
+            mks.push(L
+              .marker([o.lat, o.lng], L.extend({
+                icon: L.icon({
+                  iconUrl: 'libs/leaflet/images/M.png',
+                  iconSize: [24, 24],
+                  iconAnchor: [12, 12]
+                })
+              }, o))
               .on('click', markerClick)
-              .addTo(map);
+              .addTo(map));
           })
         });
+        map.on('zoomend', function (e) {
+          var zoom = map.getZoom();
+          if (zoom < 10) {
+            mks.forEach(function (marker) {
+              marker.setIcon(L.icon({
+                iconUrl: 'libs/leaflet/images/S1.png',
+                iconSize: [18, 18],
+                iconAnchor: [9, 9]
+              }));
+            })
+          }
+          else if (zoom < 11) {
+            mks.forEach(function (marker) {
+              marker.setIcon(L.icon({
+                iconUrl: 'libs/leaflet/images/S.png',
+                iconSize: [24, 24],
+                iconAnchor: [12, 12]
+              }));
+            })
+          }
+          else if(zoom>=11 && zoom <=12) {
+            mks.forEach(function (marker) {
+              marker.setIcon(L.icon({
+                iconUrl: 'libs/leaflet/images/M.png',
+                iconSize: [39, 39],
+                iconAnchor: [20, 20]
+              }));
+            })
+          }
+          else {
+            mks.forEach(function (marker) {
+              marker.setIcon(L.icon({
+                iconUrl: 'libs/leaflet/images/L.png',
+                iconSize: [70, 70],
+                iconAnchor: [35, 35]
+              }));
+            })
+          }
+        })
         scope.$on('destroy', function () {
           map.remove();
         })
