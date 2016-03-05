00e4a3c550743a2ed63342e84d5e81f541e8f11a
jiuyuong <jiuyuong@gmail.com>
2016-03-05 11:31:41 +0800

    修改

--- src/app/main/szgc/filters/fileurl.js
+++ src/app/main/szgc/filters/fileurl.js
@@ -8,7 +8,7 @@
   /** @ngInject */
   function fileurl(sxt){
     return function (value) {
-      return sxt.app.api + (value && value.substring(0, 1) == '~' ? value.substring(1) : value);
+      return 'http://vkde.sxtsoft.com' + (value && value.substring(0, 1) == '~' ? value.substring(1) : value);
     }
   }
 })();
