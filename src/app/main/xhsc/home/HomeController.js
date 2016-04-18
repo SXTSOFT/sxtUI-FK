f2feb2e5debe943879bf8adf493061c288074e1b
leshuangshuang <1060562807@qq.com>
2016-04-18 12:13:15 +0800

    地图模块

--- src/app/main/xhsc/home/HomeController.js
+++ src/app/main/xhsc/home/HomeController.js
@@ -1,32 +1,45 @@
 /**
  * Created by jiuyuong on 2016/3/30.
  */
-(function(){
+(function ()
+{
   'use strict';
 
   angular
     .module('app.xhsc')
-    .controller('HomeController',HomeController);
+    .controller('HomeController', HomeController);
 
   /** @ngInject */
-  function HomeController(){
-    var vm = this;
-    vm.OK = function(){
-      vm.okValue = ' okValue ';
-    }
-    vm.bass = Math.floor(Math.random() * 100);
-    vm.bassa = Math.floor(Math.random() * 100);
-    vm.showFlag = false;
-    vm.showMDBar = function(){
-      vm.showFlag = !vm.showFlag;
-    }
-    vm.images=[{'url':'app/main/xhsc/images/camera.png'},
-      {'url':'app/main/xhsc/images/camera.png'},
-      {'url':'app/main/xhsc/images/camera.png'},
-      {'url':'app/main/xhsc/images/camera.png'},
-      {'url':'app/main/xhsc/images/camera.png'},
-      {'url':'app/main/xhsc/images/camera.png'}
-    ];
-  }
+  function HomeController($scope,auth,$state,$rootScope,$timeout)
+  {
 
+    var vm = this;
+    vm.data = {};
+    vm.is = function (state) {
+      return vm.includes(state);
+    }
+    vm.markerClick = markerClick;
+    vm.querySearch = function(text){
+      var k=[];
+      if(vm.markers){
+        vm.markers.forEach(function(item){
+          if(!text || text=='' || item.title.indexOf(text)!=-1 || item.pinyin.indexOf(text)!=-1){
+            k.push(item);
+          }
+        })
+      }
+      return k;
+    }
+    vm.changeItem = function(item){
+      $timeout(function(){
+        $state.go('app.szgc.project',{pid:item.projectId, pname: item.title});
+      },200)
+
+    }
+
+    function markerClick($current){
+      appCookie.put('projects',JSON.stringify([{project_id:$current.projectId,name:$current.title}]))
+      $state.go('app.szgc.project',{pid:$current.projectId, pname: '天津星河时代'});
+    }
+  }
 })();
