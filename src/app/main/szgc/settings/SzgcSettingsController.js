f1f806215f9d74947567732f09a757f1f910149d
leshuangshuang <1060562807@qq.com>
2016-04-25 16:57:53 +0800

    版本更新2

--- src/app/main/szgc/settings/SzgcSettingsController.js
+++ src/app/main/szgc/settings/SzgcSettingsController.js
@@ -9,7 +9,7 @@
     .controller('SzgcSettingsController',SzgcSettingsController);
 
   /** @ngInject */
-  function SzgcSettingsController(profile,auth){
+  function SzgcSettingsController(profile,auth,$http, $cordovaAppVersion){
 
     var vm = this;
     vm.profile = profile.data.data;
@@ -17,5 +17,17 @@
       auth.logout();
     }
     console.log('profile',profile);
+
+    //服务器上保存版本信息
+    $http.get('http://vkde.sxtsoft.com/api/vkapi/Version')
+      .then(function (data) {
+        vm.serverAppVersion = data.data.verInfo;//服务器 版本
+
+      })
+
+  //  $cordovaAppVersion.getVersionNumber().then(function (version) {
+  //    vm.serverAppVersion = version;
+  //  });
   }
+
 })();
