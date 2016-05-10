cae97a0440f6efcb16f09ac7f6a964f676018867
jiuyuong <jiuyuong@gmail.com>
2016-05-10 11:12:16 +0800

    实测

---
+++ src/app/core/directives/fastClick.js
@@ -0,0 +1,34 @@
+/**
+ * Created by jiuyuong on 2016/5/9.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('app.core')
+    .directive('fastClick',fastClick);
+
+  /** @ngInject */
+  function fastClick() {
+    var fn = function(scope,attr) {
+      scope.$apply(function() {
+        scope.$eval(attr.fastClick);
+      });
+    };
+    return {
+      link:link
+    };
+    function link(scope,element,attr) {
+      element.on('touchstart', function(event) {
+        scope._usingTouch = true;
+        fn(scope,attr);
+      });
+
+      element.on('mousedown', function(event) {
+        if(!scope._usingTouch)
+          fn(scope,attr);
+      });
+    }
+
+  }
+})();
