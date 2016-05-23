d5c0ed676e8fd503677f0887f34bacdda1aa0174
jiuyuong <jiuyuong@gmail.com>
2016-05-23 21:24:50 +0800

    评分总表

--- src/app/main/xhsc/xhsc.api.js
+++ src/app/main/xhsc/xhsc.api.js
@@ -38,6 +38,9 @@
       },
       deleteScoreItem:function (deducScoretItemID) {
         return $http.delete('/Api/AssessmentApi/DeleteDeducScoretItem/'+deducScoretItemID);
+      },
+      queryResutTotal:function (assessmentID) {
+        return $http.get($http.url('/Api/MeasureValueApi/GetMeasureIndexResult',{AssessmentID:assessmentID}));
       }
     },
 	    /**
