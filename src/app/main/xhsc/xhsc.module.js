30f0a82e1993fb7e3d2f1a9c183c1446fe0db84d
jiuyuong <jiuyuong@gmail.com>
2016-06-06 19:12:08 +0800

    调整实测

--- src/app/main/xhsc/xhsc.module.js
+++ src/app/main/xhsc/xhsc.module.js
@@ -323,7 +323,7 @@
                 }
               });
             }],
-            template:'<div layout="column" flex ui-view="content" ></div>'
+            template:'<div layout="column" fullscreen="" flex ui-view="content" ></div>'
           }
         }
       })
@@ -341,6 +341,15 @@
       url:'/pkresult/{year}/{projectID}/{quarter}',
       views :{
         'content@app':{
+          templateUrl : 'app/main/xhsc/ys/evaluatelist_pc.html',
+          controller:'evaluatelistPcController as vm'
+        }
+      }
+    })
+    .state('app.pc.pkresult',{
+      url:'/pkresult/{year}/{projectID}/{quarter}',
+      views :{
+        'content':{
           templateUrl : 'app/main/xhsc/ys/evaluatelist_pc.html',
           controller:'evaluatelistPcController as vm'
         }
