14fdb59862163e8b37d14f8a74dcd0c4c28257fc
Emmaylj <yuanlijuan1109@163.com>
2016-06-06 21:06:40 +0800

    新增页面7

--- src/app/main/xhsc/xhsc.module.js
+++ src/app/main/xhsc/xhsc.module.js
@@ -381,6 +381,7 @@
     .state('app.xhsc.pkresult',{
       //url   :'/pkresult/{AssessmentID}/{RegionID}/{RegionName}/{AssessmentTypeID}',
       url:'/pkresult/{year}/{projectID}/{quarter}/{assessmentStage}',
+      noBack:true,
       views :{
         'content@app':{
           templateUrl : 'app/main/xhsc/ys/evaluatelist_pc.html',
