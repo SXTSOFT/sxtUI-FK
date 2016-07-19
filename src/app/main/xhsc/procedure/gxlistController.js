3b58e23ad9c274dc54ad268fdc47c548c36cbe13
Emmaylj <yuanlijuan1109@163.com>
2016-07-19 22:07:11 +0800

    楼层1

--- src/app/main/xhsc/procedure/gxlistController.js
+++ src/app/main/xhsc/procedure/gxlistController.js
@@ -20,12 +20,22 @@
       result.data.forEach(function(it){
         for(var i=0;i<it.SpecialtyChildren.length;i++){
           if(it.SpecialtyChildren[i].WPAcceptanceList.length){
+            it.SpecialtyChildren[i].WPAcceptanceList.forEach(function(t){
+              var max = 0,arr=[];
+              arr = t.ApplicableArea.split(',');
+              for(var i=0;i<arr.length;i++){
+                if(parseInt(arr[i])>max){
+                  max = arr[i];
+                }
+              }
+              t.maxRegion = max;
+            })
             vm.procedureData.push(it);
             break;
           }
         }
+
       })
-     console.log('vm',result)
     })
 
   }
