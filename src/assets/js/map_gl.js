440e69f55dccdd4ee24e6ca7314ec1a89e9daf56
jiuyuong <jiuyuong@gmail.com>
2016-07-28 22:25:11 +0800

    版本优化6

--- src/assets/js/map_gl.js
+++ src/assets/js/map_gl.js
@@ -1,15 +1,15 @@
 /**
  * map_gl 0.1
- * 
- * 
+ *
+ *
  * http://www.sxt.com.cn/
- * 
+ *
  * Copyright 2016, jiuyuong
- * 
+ *
  * http://www.sxt.com.cn/
- * 
+ *
  * Licensed under MIT
- * 
+ *
  * Released on: July 28, 2016
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.mapboxgl = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -1110,7 +1110,7 @@ module.exports={"name":"mapbox-gl","description":"A WebGL interactive maps libra
                     });
                     ctx.map.addLayer({
                         "id": cold + 'lay1',
-                        
+
                         "source": cold,
                         "filter": ["all", ["==", "$type", "Point"], ["in", "Status", 1, 4]],
                         "type": "circle",
@@ -1187,7 +1187,7 @@ module.exports={"name":"mapbox-gl","description":"A WebGL interactive maps libra
                 ctx.map.removeSource(cold);
             },
             addEventListeners:function () {
-                
+
             },
             changeMode:function (mode,op) {
                 if (!ctx.map)return;
@@ -1392,7 +1392,7 @@ module.exports={"name":"mapbox-gl","description":"A WebGL interactive maps libra
             self.el = null;
         }
     }
-    
+
     L.glProject = Project;
 })(window);
 //# sourceMappingURL=maps/map_gl.js.map
