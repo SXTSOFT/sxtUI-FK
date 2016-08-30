dd5996c7dc88dcce4b3eaef49d706f1629334ccb
shaoshun <170243414@qq.com>
2016-08-30 22:59:00 +0800

    Merge branch 'xhsc' of https://github.com/SXTSOFT/sxtUI-FK into xhsc
    # Conflicts:
    #	src/app/main/xhsc/directive/sxtRecheckMap.js
    #	src/app/main/xhsc/directive/sxtRecheckPopup.js
    #	src/app/main/xhsc/procedure/gxzg.html
    #	src/app/main/xhsc/procedure/gxzgController.js

--- src/app/main/xhsc/procedure/gxlistController.js
+++ src/app/main/xhsc/procedure/gxlistController.js
@@ -14,38 +14,48 @@
     vm.projectId = $stateParams.projectId;
     vm.role=$stateParams.role;
 
-    remote.Procedure.queryProcedure().then(function(result){
-      vm.procedureData = [];
-      vm.list=[];
-      result.data.forEach(function(it){
-        var name = it.SpecialtyName;
-        var plist=[];
-        it.SpecialtyChildren.forEach(function(t){
-          if(t.WPAcceptanceList.length) {
+    vm.selectSpecialty=function(item){
+        if (item.SpecialtyChildren&&item.SpecialtyChildren.length){
+          vm.gxClassType=item.SpecialtyChildren;
+        }
+    }
+    vm.acceptanceItem=[];
+    vm.selectSpecialtyLow=function(item){
+      vm.acceptanceItem=item.WPAcceptanceList?item.WPAcceptanceList:[];
+    }
 
-            t.WPAcceptanceList.forEach(function (_t) {
-              var max = 0, arr = [];
-              var idx = _t.ApplicableArea.indexOf(',');
-              if (idx == -1) {
-                _t.maxRegion = _t.ApplicableArea;
-              } else {
-                arr = _t.ApplicableArea.split(',');
-                for (var i = 0; i < arr.length; i++) {
-                  if (parseInt(arr[i]) > max) {
-                    max = arr[i];
-                  }
-                }
-                _t.maxRegion = max;
-              }
-              plist.push(_t);
-            })
-          }
-        })
-        vm.procedureData.push({name:name,rows:plist});
-//console.log('plist', vm.procedureData)
-        //vm.procedureData.push({name:it.SpecialtyName,rows:vm.wpalist});
-      })
-    //  ui-sref="app.xhsc.gx.zjhouseChoose({role:vm.role,acceptanceItemID:i.AcceptanceItemID,projectId:vm.projectId,acceptanceItemName:i.AcceptanceItemName,areaId:vm.areaId,maxRegion:i.maxRegion})"
+    remote.Procedure.queryProcedure().then(function(result){
+     vm.data=result.data;
+      console.log(vm.data);
+//      vm.procedureData = [];
+//      vm.list=[];
+//      result.data.forEach(function(it){
+//        var name = it.SpecialtyName;
+//        var plist=[];
+//        it.SpecialtyChildren.forEach(function(t){
+//          if(t.WPAcceptanceList.length) {
+//
+//            t.WPAcceptanceList.forEach(function (_t) {
+//              var max = 0, arr = [];
+//              var idx = _t.ApplicableArea.indexOf(',');
+//              if (idx == -1) {
+//                _t.maxRegion = _t.ApplicableArea;
+//              } else {
+//                arr = _t.ApplicableArea.split(',');
+//                for (var i = 0; i < arr.length; i++) {
+//                  if (parseInt(arr[i]) > max) {
+//                    max = arr[i];
+//                  }
+//                }
+//                _t.maxRegion = max;
+//              }
+//              plist.push(_t);
+//            })
+//          }
+//        })
+//        vm.procedureData.push({name:name,rows:plist});
+//        //vm.procedureData.push({name:it.SpecialtyName,rows:vm.wpalist});
+//      })
     })
     vm.choosego = function(i){
       if(!vm.role){
