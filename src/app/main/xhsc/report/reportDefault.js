3cd9ba5cb7ab44cd116bfc351c527bc6a4ea984b
shaoshun <170243414@qq.com>
2016-09-08 22:50:42 +0800

    Merge branch 'xhsc' of https://github.com/SXTSOFT/sxtUI-FK into xhsc
    # Conflicts:
    #	src/app/main/xhsc/directive/sxtRecheckMap.js
    #	src/app/main/xhsc/directive/sxtRecheckPopup.js
    #	src/app/main/xhsc/procedure/gxzg.html
    #	src/app/main/xhsc/procedure/gxzgController.js

---
+++ src/app/main/xhsc/report/reportDefault.js
@@ -0,0 +1,99 @@
+/**
+ * Created by lss on 2016/8/16.
+ */
+(function(){
+  'use strict';
+
+  angular
+    .module('app.xhsc')
+    .controller('reportDefault',reportDefault);
+
+  /** @ngInject */
+  function reportDefault(remote,$state,$q,utils,api){
+    var vm=this;
+    var pro=[
+      remote.Procedure.authorityByUserId(),
+      remote.Project.getMap()
+    ]
+    api.setNetwork(0).then(function(){
+      $q.all(pro).then(function(r){
+        var res=r[0];
+        var result=r[1];
+        if (res&&res.data&&res.data.length){
+          vm.role=res.data[0].MemberType;
+        }else {
+          vm.role=0;
+        }
+        if(result&&result.data.length){
+          result.data.forEach(function (m) {
+            m.AssessmentID='scsl'+ m.ProjectID+'_'+vm.role;
+            m.AssessmentSubject= m.ProjectName;
+          });
+          vm.projects=result.data;
+        }
+      }).catch(function(k){
+        utils.alert("数据加载出错！");
+      });
+      //查看整改报表
+      vm.Lookintoys = function(item){
+        $state.go('app.xhsc.gx.gxzgreport',{InspectionId:item.InspectionId, acceptanceItemID:item.AcceptanceItemID,acceptanceItemName:item.AcceptanceItemName,projectId:item.ProjectID});
+      }
+      //查看验收报告
+      vm.Lookintoys = function(item){
+        $state.go('app.xhsc.gx.gxzgreport',{InspectionId:item.InspectionId, acceptanceItemID:item.AcceptanceItemID,acceptanceItemName:item.AcceptanceItemName,projectId:item.ProjectID});
+      }
+      //验收报告
+      remote.Procedure.getInspections(31).then(function(r){
+        vm.Inspections=[];
+        r.data.forEach(function(o){
+          if (o.Sign!=8){
+            vm.Inspections.push(o);
+          }
+        });
+      });
+      vm.go=function(item){
+        function callBack(r){
+          if (r&& r.data){
+            var areas=r.data;
+            var  routeData={
+              projectId:item.ProjectID,
+              assessmentID:item.AssessmentID,
+              role:vm.role,
+              isReport:1
+            };
+            if (angular.isArray(areas)&&areas.length>1){
+              $state.go("app.xhsc.scsl.chooseArea",routeData)
+            }else {
+              $state.go("app.xhsc.scsl.sclist",angular.extend(routeData,{area:areas[0].RegionID}));
+            }
+          }
+        }
+        remote.Project.GetAreaChildenbyID(item.ProjectID).then(callBack);
+      }
+
+      remote.Report.Summary().then(function(r){
+          vm.report= r.data? r.data:[];
+      });
+
+
+      vm.selectSpecialty=function(item){
+        vm.gxClassType=item.SpecialtyChildren;
+        vm.acceptanceItem=[];
+      }
+      vm.acceptanceItem=[];
+      vm.selectSpecialtyLow=function(item){
+        vm.acceptanceItem=item.WPAcceptanceList?item.WPAcceptanceList:[];
+      }
+
+      vm.comeBack=function(){
+        vm.acceptanceItem=[];
+      }
+      remote.Procedure.queryProcedure().then(function(result){
+        vm.data=result.data;
+      })
+      vm.choosego = function(i){
+        $state.go('app.xhsc.report.ysReport',{acceptanceItemID: i.AcceptanceItemID})
+      }
+    });
+  }
+})();
