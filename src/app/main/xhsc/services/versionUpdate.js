e3bdf6e2d9312a45c5b55047f7041969e819a94c
shaoshun <170243414@qq.com>
2016-09-22 17:07:51 +0800

    Merge branch 'xhsc' of https://github.com/SXTSOFT/sxtUI-FK into xhsc
    # Conflicts:
    #	src/app/main/xhsc/directive/sxtRecheckMap.js
    #	src/app/main/xhsc/directive/sxtRecheckPopup.js
    #	src/app/main/xhsc/procedure/gxzg.html
    #	src/app/main/xhsc/procedure/gxzgController.js

--- src/app/main/xhsc/services/versionUpdate.js
+++ src/app/main/xhsc/services/versionUpdate.js
@@ -10,7 +10,7 @@
 
   function versionUpdate($mdDialog, $cordovaFileTransfer, $window,$http) {
 
-    var version = '1.1.2';
+    var version = '1.1.01';
     this.version = version;
     function versionToNumber(version) {
       var n = version.split('.');
