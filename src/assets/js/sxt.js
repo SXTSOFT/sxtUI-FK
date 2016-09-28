af8647acc1d9ec0ee5f4ba363fae28d7c48e4cd5
Emmaylj <yuanlijuan1109@163.com>
2016-09-28 16:35:56 +0800

    yanse

--- src/assets/js/sxt.js
+++ src/assets/js/sxt.js
@@ -37,8 +37,10 @@
     app: {
 
       couchDb:'http://ggem.sxtsoft.com:5984',
-      //api:'http://xhszgc.sxtsoft.com:7091',
+      //api:'http://vkde.sxtsoft.com:8091',
       api:'http://xhszgc.sxtsoft.com:9091',
+      //api:'http://xhszgc.sxtsoft.com:7091',
+      //api:'http://xhszgc.sxtsoft.com:9091',
       //api:'http://10.0.8.183:9091',
       //fs:'http://xhszgc.sxtsoft.com:8090'
       fs:'http://xhszgc.sxtsoft.com:9090'
