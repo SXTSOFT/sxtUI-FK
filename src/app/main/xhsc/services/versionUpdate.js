acf471bbb9a37e907173fd6fd05afcff7814a7df
jiuyuong <jiuyuong@gmail.com>
2016-10-14 11:23:17 +0800

    加版本

--- src/app/main/xhsc/services/versionUpdate.js
+++ src/app/main/xhsc/services/versionUpdate.js
@@ -10,7 +10,7 @@
 
   function versionUpdate($mdDialog, $cordovaFileTransfer, $window,$http) {
 
-    var version = '1.1.3';
+    var version = '1.1.6';
     this.version = version;
     function versionToNumber(version) {
       var n = version.split('.');
