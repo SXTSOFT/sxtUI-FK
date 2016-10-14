43bda664a04e52baa56389c4ef83a2aec4158775
shaoshun <170243414@qq.com>
2016-10-14 20:03:05 +0800

    Merge branch 'xhsc' of https://github.com/SXTSOFT/sxtUI-FK into xhsc
    # Conflicts:
    #	src/app/main/xhsc/directive/sxtRecheckMap.js
    #	src/app/main/xhsc/directive/sxtRecheckPopup.js
    #	src/app/main/xhsc/procedure/gxzg.html
    #	src/app/main/xhsc/procedure/gxzgController.js

--- src/assets/js/sxt.js
+++ src/assets/js/sxt.js
@@ -39,12 +39,12 @@
       couchDb:'http://ggem.sxtsoft.com:5984',
       //api:'http://vkde.sxtsoft.com:8091',
       //api:'http://xhszgc.sxtsoft.com:9091',
-      //api:'http://emp.chngalaxy.com:9091',
-      api:'http://10.0.9.79:7091',
+      api:'http://emp.chngalaxy.com:9091',
+      //api:'http://10.0.9.79:7091',
       //api:'http://xhszgc.sxtsoft.com:9091',
       //api:'http://10.0.8.183:9091',
       //fs:'http://xhszgc.sxtsoft.com:8090'
-      fs:'http://10.0.9.79:7090'
+      fs:'http://emp.chngalaxy.com:9090'
     },
     angular:{
 
