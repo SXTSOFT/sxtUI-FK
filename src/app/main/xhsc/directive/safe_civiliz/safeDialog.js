c0eb5fc971ee8495891e51ad44d207540a552b49
shaoshun <170243414@qq.com>
2016-10-24 16:22:19 +0800

    Merge branch 'xhsc' of https://github.com/SXTSOFT/sxtUI-FK into xhsc
    # Conflicts:
    #	src/app/main/xhsc/directive/sxtRecheckMap.js
    #	src/app/main/xhsc/directive/sxtRecheckPopup.js
    #	src/app/main/xhsc/procedure/gxzg.html
    #	src/app/main/xhsc/procedure/gxzgController.js

---
+++ src/app/main/xhsc/directive/safe_civiliz/safeDialog.js
@@ -0,0 +1,103 @@
+/**
+ * Created by lss on 2016/10/24.
+ */
+/**
+ * Created by emma on 2016/6/7.
+ */
+(function(){
+  'use strict';
+
+  angular
+    .module('app.xhsc')
+    .directive('safeDialog',safeDialog);
+
+  /**@ngInject*/
+  function safeDialog($state,$timeout,remote,utils,auth,sxt,$mdDialog,api){
+    return {
+      scope:{
+        dialogShow:'=',
+        dialogData:'=',
+        dialogSure:'@',
+        dialogMsg:'@',
+        callBack:"&"
+      },
+      templateUrl:"app/main/xhsc/directive/safe_civiliz/safeDialog.html",
+      link:link,
+      controller:function($scope){
+        $scope.description="";
+        $scope. percentage="";
+      }
+    }
+    function link(scope,element,attr,ctrl){
+      $(element).appendTo('body');
+      $('.my-dialog-mask',element).fadeOut();
+      scope.$watch('dialogShow',function(){
+        if(scope.dialogShow){
+          $('.my-dialog-mask',element).fadeIn();
+          scope.description="";
+          scope. percentage=100;
+        }
+        if(scope.dialogSure != '报验'){
+          scope.logOut= true;
+        }
+      })
+      scope.$watch('dialogData',function(){
+        if(!scope.dialogData) return;
+        var msg='';
+        scope.dialogData.Rows.forEach(function(t){
+          msg += t.projectTree+'，';
+        })
+        scope.dialogMsg = msg + scope.dialogData.acceptanceItemName+ '工序已自检完毕，请监理验收';
+      })
+      scope.cancel= function(){
+        scope.dialogShow = false;
+        $('.my-dialog-mask',element).fadeOut();
+      }
+      scope.submit = function(evt){
+        var parentEl = angular.element(document.body);
+        $mdDialog.show({
+          parent: parentEl,
+          targetEvent: evt,
+          template:'<md-dialog   ng-cloak><md-dialog-content> <md-progress-circular md-mode="indeterminate"></md-progress-circular> 正在提交数据，请稍候……</md-dialog-content></md-dialog>',
+          controller: DialogController
+        });
+        function DialogController($scope) {
+          scope.dialogShow = false;
+          scope.areaIds=[];
+          $('.my-dialog-mask',element).fadeOut();
+          if(scope.dialogSure == '报验'){
+            var percentage= scope.percentage?scope.percentage:100
+            scope.dialogData.Rows.forEach(function(t){
+              scope.areaIds.push( {
+                AreaID:t.RegionID,
+                Describe:scope.description,
+                Percentage:percentage
+              });
+            })
+            var params ={
+              AcceptanceItemID:scope.dialogData.acceptanceItemID,
+              AreaList:scope.areaIds,
+              Describe:scope.description,
+              Percentage:percentage,
+              id:sxt.uuid()
+            }
+            api.setNetwork(0).then(function(){
+              remote.Procedure.postInspection(params).then(function(result){
+                if (result.data.ErrorCode==0) {
+                  scope.callBack();
+                }
+              }).catch(function(){
+                $mdDialog.cancel();
+              });
+            });
+          }else{
+            $mdDialog.hide();
+          }
+        }
+      }
+      scope.$on('$destroy', function () {
+        $(element).remove()
+      });
+    }
+  }
+})();
