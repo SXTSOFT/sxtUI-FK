ce0b162fb53129dd4b38f6f633d161fa175c58b8
GzHuangQingFeng <1179060151@qq.com>
2016-11-01 19:15:46 +0800

    新增：材料计划不合格退场

--- src/app/main/materialYs/component/materialPlanYs-list.controller.js
+++ src/app/main/materialYs/component/materialPlanYs-list.controller.js
@@ -15,11 +15,13 @@
   function materialPlanList($rootScope,$stateParams,api,utils,$state,auth){
     var vm = this;
     $rootScope.title = 'aaa';
-    ///api.setNetwork(1).then(function(){});
     var user = auth.current();
     var status = user.Role.MemberType==0?17:110;
-    api.xhsc.materialPlan.getMaterialPlanBatch($stateParams.id,status).then(function (r) {
-      vm.planList = r.data;
+
+    api.setNetwork(1).then(function(){
+      api.xhsc.materialPlan.getMaterialPlanBatch($stateParams.id,status).then(function (r) {
+        vm.planList = r.data;
+      });
     });
 
   }
--- src/app/main/materialYs/materialys.api.js
+++ src/app/main/materialYs/materialys.api.js
@@ -18,7 +18,7 @@
         getMaterialPlanBatch: $http.db({
           _id: 'materialPlan',
           idField: 'Id',
-          dataType: 1
+          dataType: 3
         }).bind(function (sectionId,status) {
           return $http.get($http.url('/api/MaterialPlan/GetMaterialPlansBatchBySectionId',{sectionId:sectionId,status:status}));
         }),
