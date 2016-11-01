226ee880e5f77d365d4cb880e224358c94fe9c27
GzHuangQingFeng <1179060151@qq.com>
2016-11-01 19:41:17 +0800

    新增：材料计划不合格退场

--- src/app/main/materialYs/materialys.api.js
+++ src/app/main/materialYs/materialys.api.js
@@ -18,7 +18,7 @@
         getMaterialPlanBatch: $http.db({
           _id: 'materialPlan',
           idField: 'Id',
-          dataType: 3
+          dataType: 1
         }).bind(function (sectionId,status) {
           return $http.get($http.url('/api/MaterialPlan/GetMaterialPlansBatchBySectionId',{sectionId:sectionId,status:status}));
         }),
@@ -28,7 +28,6 @@
           dataType:3
         }).bind(function (mpid) {
           return $http.get($http.url('/api/MaterialPlan/GetMaterialPlanAsync?mpid='+mpid));
-
         }),
         PostCheckInfo: $http.db({
           _id: 'materialPlan',
