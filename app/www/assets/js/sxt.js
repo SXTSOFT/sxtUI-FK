041277ae440383baacab4c69b22b4396903ad2b1
shaoshunliu <170243414@qq.com>
2016-11-05 09:55:28 +0800

    sc标准化

--- app/www/assets/js/sxt.js
+++ app/www/assets/js/sxt.js
@@ -35,16 +35,11 @@
   win.sxt = {
     version: '1.1.12',
     app: {
-
-      couchDb:'http://ggem.sxtsoft.com:5984',
-      //api:'http://vkde.sxtsoft.com:8091',
-      //api:'http://xhszgc.sxtsoft.com:9091',
       api:'http://emp.chngalaxy.com:9091',
-      //api:'http://xhszgc.sxtsoft.com:7091',
-      //api:'http://xhszgc.sxtsoft.com:9091',
-      //api:'http://10.0.8.183:9091',
-      //fs:'http://xhszgc.sxtsoft.com:8090'
+      // api: 'http://10.0.9.79:7091',
+
       fs:'http://emp.chngalaxy.com:9090'
+      // fs:'http://10.0.9.79:7090'
     },
     angular:{
 
