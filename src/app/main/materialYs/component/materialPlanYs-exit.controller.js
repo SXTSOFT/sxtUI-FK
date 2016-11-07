59939acd5755c44fceb319a60b6d0b8a342914d9
Emmaylj <yuanlijuan1109@163.com>
2016-11-07 14:26:09 +0800

    11-7

--- src/app/main/materialYs/component/materialPlanYs-exit.controller.js
+++ src/app/main/materialYs/component/materialPlanYs-exit.controller.js
@@ -22,7 +22,6 @@
     });
 
     var sendCheckResult = $rootScope.$on('sendGxResult',function() {
-      console.log(vm.data)
       api.xhsc.materialPlan.PostExitInfo(vm.data).then(function (r) {
         utils.alert('提交成功!');
       })
