a9a69ca87a5e524bd66defeb090df012c029c965
shaoshunliu <170243414@qq.com>
2016-11-09 02:26:55 +0800

    sc标准化

--- src/app/main/xhsc/procedure/gxhousechooseController.js
+++ src/app/main/xhsc/procedure/gxhousechooseController.js
@@ -156,88 +156,34 @@
         result.Children.forEach(function(d){
           if (d.RegionID==areaId){
             d.selected=true;
-          }
-          filterOrSetting(status,d);
-          d.projectTree =  result.RegionName;
-          d.projectTitle = result.RegionName + d.RegionName;
-          d.Children && d.Children.forEach(function(c){
-            c.floors=[];
-            filterOrSetting(status,c)
-            c.projectTree = d.projectTree + c.RegionName;
-            c.checked = false;
-            vm.building.push(c);
-            c.Children && c.Children.forEach(function(r){
-              r.projectTree = c.projectTree + r.RegionName;
-              r.checked = false;
-              filterOrSetting(status,r);
-              vm.floors=vm.floors?vm.floors:[];
-              vm.floors.push(r);
-              if (! r.Children||!r.Children.length){
-                c.floors.push(r);
-              }
-              r.Children && r.Children.forEach(function(_r){
-                _r.projectTree = r.projectTree + _r.RegionName;
-                _r.checked = false;
-                filterOrSetting(status,_r);
+            filterOrSetting(status,d);
+            d.projectTree =  result.RegionName;
+            d.projectTitle = result.RegionName + d.RegionName;
+            d.Children && d.Children.forEach(function(c){
+              c.floors=[];
+              filterOrSetting(status,c)
+              c.projectTree = d.projectTree + c.RegionName;
+              c.checked = false;
+              vm.building.push(c);
+              c.Children && c.Children.forEach(function(r){
+                r.projectTree = c.projectTree + r.RegionName;
+                r.checked = false;
+                filterOrSetting(status,r);
+                vm.floors=vm.floors?vm.floors:[];
+                vm.floors.push(r);
+                if (! r.Children||!r.Children.length){
+                  c.floors.push(r);
+                }
+                r.Children && r.Children.forEach(function(_r){
+                  _r.projectTree = r.projectTree + _r.RegionName;
+                  _r.checked = false;
+                  filterOrSetting(status,_r);
+                })
               })
             })
-          })
+          }
         })
         vm.houses =  result.Children;
-        // function DynamicItems() {
-        //   /**
-        //    * @type {!Object<?Array>} Data pages, keyed by page number (0-index).
-        //    */
-        //   this.loadedPages = {};
-        //
-        //   /** @type {number} Total number of items. */
-        //   this.numItems = 0;
-        //
-        //   /** @const {number} Number of items to fetch per request. */
-        //   this.PAGE_SIZE = 1;
-        //
-        //   this.fetchNumItems_();
-        // };
-        // // Required.
-        // DynamicItems.prototype.getItemAtIndex = function (index) {
-        //   var pageNumber = Math.floor(index / this.PAGE_SIZE);
-        //   var page = this.loadedPages[pageNumber];
-        //
-        //   if (page) {
-        //     return page[index % this.PAGE_SIZE];
-        //   } else if (page !== null) {
-        //     this.fetchPage_(pageNumber);
-        //   }
-        // };
-        // // Required.
-        // DynamicItems.prototype.getLength = function () {
-        //   return this.numItems;
-        // };
-        //
-        // DynamicItems.prototype.fetchPage_ = function (pageNumber) {
-        //   // Set the page to null so we know it is already being fetched.
-        //   this.loadedPages[pageNumber] = null;
-        //
-        //   // For demo purposes, we simulate loading more items with a timed
-        //   // promise. In real code, this function would likely contain an
-        //   // $http request.
-        //   $timeout(angular.noop, 0).then(angular.bind(this, function () {
-        //     this.loadedPages[pageNumber] = [];
-        //     var pageOffset = pageNumber * this.PAGE_SIZE;
-        //     for (var i = pageOffset; i < pageOffset + this.PAGE_SIZE; i++) {
-        //       if (vm.building[i]) {
-        //         this.loadedPages[pageNumber].push(vm.building[i]);
-        //       }
-        //     }
-        //   }));
-        // };
-        //
-        // DynamicItems.prototype.fetchNumItems_ = function () {
-        //   $timeout(angular.noop, 0).then(angular.bind(this, function () {
-        //     this.numItems = vm.building.length;
-        //   }));
-        // };
-        // vm.dynamicItems = new DynamicItems();
         return vm.houses;
       });
     }
