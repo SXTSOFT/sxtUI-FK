482c02e0326a8d66f9b092e2afa075bcff4d6261
shaoshunliu <170243414@qq.com>
2016-11-08 15:49:25 +0800

    sc标准化

--- src/app/main/xhsc/procedure/gxhousechooseController.js
+++ src/app/main/xhsc/procedure/gxhousechooseController.js
@@ -184,60 +184,60 @@
           })
         })
         vm.houses =  result.Children;
-        function DynamicItems() {
-          /**
-           * @type {!Object<?Array>} Data pages, keyed by page number (0-index).
-           */
-          this.loadedPages = {};
-
-          /** @type {number} Total number of items. */
-          this.numItems = 0;
-
-          /** @const {number} Number of items to fetch per request. */
-          this.PAGE_SIZE = 5;
-
-          this.fetchNumItems_();
-        };
-        // Required.
-        DynamicItems.prototype.getItemAtIndex = function (index) {
-          var pageNumber = Math.floor(index / this.PAGE_SIZE);
-          var page = this.loadedPages[pageNumber];
-
-          if (page) {
-            return page[index % this.PAGE_SIZE];
-          } else if (page !== null) {
-            this.fetchPage_(pageNumber);
-          }
-        };
-        // Required.
-        DynamicItems.prototype.getLength = function () {
-          return this.numItems;
-        };
-
-        DynamicItems.prototype.fetchPage_ = function (pageNumber) {
-          // Set the page to null so we know it is already being fetched.
-          this.loadedPages[pageNumber] = null;
-
-          // For demo purposes, we simulate loading more items with a timed
-          // promise. In real code, this function would likely contain an
-          // $http request.
-          $timeout(angular.noop, 0).then(angular.bind(this, function () {
-            this.loadedPages[pageNumber] = [];
-            var pageOffset = pageNumber * this.PAGE_SIZE;
-            for (var i = pageOffset; i < pageOffset + this.PAGE_SIZE; i++) {
-              if (vm.building[i]) {
-                this.loadedPages[pageNumber].push(vm.building[i]);
-              }
-            }
-          }));
-        };
-
-        DynamicItems.prototype.fetchNumItems_ = function () {
-          $timeout(angular.noop, 0).then(angular.bind(this, function () {
-            this.numItems = vm.building.length;
-          }));
-        };
-        vm.dynamicItems = new DynamicItems();
+        // function DynamicItems() {
+        //   /**
+        //    * @type {!Object<?Array>} Data pages, keyed by page number (0-index).
+        //    */
+        //   this.loadedPages = {};
+        //
+        //   /** @type {number} Total number of items. */
+        //   this.numItems = 0;
+        //
+        //   /** @const {number} Number of items to fetch per request. */
+        //   this.PAGE_SIZE = 1;
+        //
+        //   this.fetchNumItems_();
+        // };
+        // // Required.
+        // DynamicItems.prototype.getItemAtIndex = function (index) {
+        //   var pageNumber = Math.floor(index / this.PAGE_SIZE);
+        //   var page = this.loadedPages[pageNumber];
+        //
+        //   if (page) {
+        //     return page[index % this.PAGE_SIZE];
+        //   } else if (page !== null) {
+        //     this.fetchPage_(pageNumber);
+        //   }
+        // };
+        // // Required.
+        // DynamicItems.prototype.getLength = function () {
+        //   return this.numItems;
+        // };
+        //
+        // DynamicItems.prototype.fetchPage_ = function (pageNumber) {
+        //   // Set the page to null so we know it is already being fetched.
+        //   this.loadedPages[pageNumber] = null;
+        //
+        //   // For demo purposes, we simulate loading more items with a timed
+        //   // promise. In real code, this function would likely contain an
+        //   // $http request.
+        //   $timeout(angular.noop, 0).then(angular.bind(this, function () {
+        //     this.loadedPages[pageNumber] = [];
+        //     var pageOffset = pageNumber * this.PAGE_SIZE;
+        //     for (var i = pageOffset; i < pageOffset + this.PAGE_SIZE; i++) {
+        //       if (vm.building[i]) {
+        //         this.loadedPages[pageNumber].push(vm.building[i]);
+        //       }
+        //     }
+        //   }));
+        // };
+        //
+        // DynamicItems.prototype.fetchNumItems_ = function () {
+        //   $timeout(angular.noop, 0).then(angular.bind(this, function () {
+        //     this.numItems = vm.building.length;
+        //   }));
+        // };
+        // vm.dynamicItems = new DynamicItems();
         return vm.houses;
       });
     }
