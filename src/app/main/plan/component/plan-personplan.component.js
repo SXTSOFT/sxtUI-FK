6ae1e7195ce7fbae46cc5ff5b92fb6e451ffe5de
Emmaylj <yuanlijuan1109@163.com>
2016-11-24 16:41:34 +0800

    个人计划

--- src/app/main/plan/component/plan-personplan.component.js
+++ src/app/main/plan/component/plan-personplan.component.js
@@ -13,33 +13,45 @@
     });
 
   /**@ngInject*/
-  function personPlanController($scope,$rootScope,moment,api,$mdDialog){
+  function personPlanController($scope,$rootScope,moment,api,$mdDialog,$timeout){
     var vm = this;
-    var now = new Date();
-    vm.tasksList = [];
-    api.plan.BuildPlan.personPlans().then(function(res){
-      vm.tdata = res.data;
-      vm.tdata.forEach(function(_r){
-        _r.PlaningTasks.forEach(function(r){
-          r.sdate =r.ScheduledStartTime&& moment(r.ScheduledStartTime).toDate();
-          r.edate =r.ScheduledEndTime&& moment(r.ScheduledEndTime).toDate();
+    load();
+    function load() {
+      var now = moment(new Date().toISOString().substring(0,10)).toDate();
+      vm.tasksList = [];
+      api.plan.BuildPlan.personPlans().then(function (res) {
+        vm.tdata = res.data;
+        vm.tdata.forEach(function (_r) {
+          _r.showNoPlan = _r.NoPlanTasks.length ? true : false;
+          _r.PlaningTasks.forEach(function (r) {
+            r.sdate = r.ActualStartTime && moment(r.ActualStartTime.substring(0, 10)).toDate() || r.ScheduledStartTime && moment(r.ScheduledStartTime.substring(0, 10)).toDate();
+            r.edate = r.ActualEndTime && moment(r.ActualEndTime.substring(0, 10)).toDate() || r.ScheduledEndTime && moment(r.ScheduledEndTime.substring(0, 10)).toDate();
+          })
+        })
+        vm.data = vm.tdata;
+        vm.data && vm.data.forEach(function (_r) {
+          var temp = {
+            Name: _r.Name
+          };
+          temp.tasks = _r.PlaningTasks.filter(function (r) {
+            return (r.edate && (r.sdate <= now && r.edate >= now)) || (!r.edate && r.sdate <= now);
+          })
+          vm.tasksList.push(temp);
+        })
+        vm.tasksList.forEach(function(task){
+          task.tasks.forEach(function(_task){
+            _task.showBtn = true;
+          })
         })
       })
-      vm.data = vm.tdata;
-      vm.data&&vm.data.forEach(function(_r){
-        var temp = _r.PlaningTasks.filter(function(r){
-          return (r.sdate&&r.edate&&(r.sdate.getFullYear()<= now.getFullYear()&& r.edate.getFullYear() >= now.getFullYear())&&r.sdate.getDate()<=now.getDate()&& r.edate.getDate()>=now.getDate()&&r.sdate.getMonth()<= now.getMonth()&& r.edate.getMonth()>= now.getMonth())
-        })
-        vm.tasksList.push(temp)
-      })
-      console.log(vm.tasksList)
-    })
+    }
     $scope.$watch('vm.data',function(){
       if(vm.data){
         $scope.onlyWeekendsPredicate = function(date){
           for(var j=0;j<vm.data.length;j++){
             for(var i=0;i<vm.data[j].PlaningTasks.length;i++){
-              if(vm.data[j].PlaningTasks[i].edate&&vm.data[j].PlaningTasks[i].sdate&&(vm.data[j].PlaningTasks[i].sdate.getDate()<=date.getDate()&&vm.data[j].PlaningTasks[i].edate.getDate()>=date.getDate())&&(vm.data[j].PlaningTasks[i].sdate.getFullYear()<= date.getFullYear()&&vm.data[j].PlaningTasks[i].edate.getFullYear()>= date.getFullYear()  )&& (vm.data[j].PlaningTasks[i].sdate.getMonth() <= date.getMonth()&&vm.data[j].PlaningTasks[i].edate.getMonth() >= date.getMonth())){
+              if(vm.data[j].PlaningTasks[i].edate&&vm.data[j].PlaningTasks[i].sdate <= date && vm.data[j].PlaningTasks[i].edate >= date||
+              !vm.data[j].PlaningTasks[i].edate&&vm.data[j].PlaningTasks[i].sdate <= date){
                 return true;
               }
             }
@@ -47,25 +59,24 @@
         }
         vm.loading = true;
       }
+    },true);
+    $scope.$watch('vm.myDate',function(){
+      if(!vm.myDate){
+        vm.myDate = new Date();
+        $timeout(function(){
+          vm.myDate = new Date();
+        },500)
+      }
     })
-    //$scope.onlyWeekendsPredicate = function(date){
-    //  for(var j=0;j<vm.data.length;j++){
-    //    for(var i=0;i<vm.data[j].tasks.length;i++){
-    //      if((vm.data[j].tasks[i].sdate.getDate()<=date.getDate()&&vm.data[j].tasks[i].edate.getDate()>=date.getDate())&&(vm.data[j].tasks[i].sdate.getFullYear() >= date.getFullYear()&&vm.data[j].tasks[i].edate.getFullYear() <= date.getFullYear()  )&& (vm.data[j].tasks[i].sdate.getMonth() >= date.getMonth()&&vm.data[j].tasks[i].edate.getMonth() <= date.getMonth())){
-    //        return true;
-    //      }
-    //    }
-    //  }
-    //}
     $rootScope.$on('md-calendar-change', function(event,data) {
       vm.tasksList = [];
+      console.log(data)
       vm.data && vm.data.forEach(function (_r) {
         var temp ={
-          Name:_r.Name,
-          tasks:[]
+          Name:_r.Name
         };
-         temp.data = _r.PlaningTasks.filter(function (r) {
-          return (r.sdate && r.edate && (r.sdate.getFullYear() <= data.getFullYear() && r.edate.getFullYear() >= data.getFullYear()) && r.sdate.getDate() <= data.getDate() && r.edate.getDate() >= data.getDate() && r.sdate.getMonth()<= data.getMonth() && r.edate.getMonth() >= data.getMonth())
+         temp.tasks = _r.PlaningTasks.filter(function (r) {
+           return r.edate&&(r.sdate <=data && r.edate>=data)||!r.edate&& r.sdate<=data;
         })
         vm.tasksList.push(temp)
       });
@@ -74,12 +85,25 @@
     vm.start = function(t){
       var time = new Date();
       api.plan.Task.start(t.Id,true,time).then(function(r){
-        var f = r.data.find(function(_r){
-          return _r.Id == t.Id;
+        vm.data.forEach(function(tt){
+          tt.PlaningTasks.forEach(function(_t){
+            var f = r.data.find(function(_r){
+              return _r.Id == _t.Id;
+            })
+            if(f){
+              t.State = f.State;
+            }
+          })
+          tt.NoPlanTasks.forEach(function(_t){
+            var f = r.data.find(function(_r){
+              return _r.Id == _t.Id;
+            })
+            if(f){
+              t.State = f.State;
+            }
+          })
         })
-        if(f){
-          t.State = f.State;
-        }
+
       })
     }
     vm.end = function(t){
@@ -92,11 +116,30 @@
           .ok('确定')
           .cancel('取消')
       ).then(function(res){
-        console.log(res)
+        api.plan.Task.end(t.Id,true,time,res).then(function(r){
+          vm.loading = false;
+          load();
+          //vm.data.forEach(function(tt){
+          //  tt.PlaningTasks.forEach(function(_t){
+          //    var f = r.data.find(function(_r){
+          //      return _r.Id == _t.Id;
+          //    })
+          //    if(f){
+          //      t.State = f.State;
+          //    }
+          //  })
+          //  tt.NoPlanTasks.forEach(function(_t){
+          //    var f = r.data.find(function(_r){
+          //      return _r.Id == _t.Id;
+          //    })
+          //    if(f){
+          //      t.State = f.State;
+          //    }
+          //  })
+          //})
+        })
       })
-      api.plan.Task.end(t.Id,true,time).then(function(r){
 
-      })
     }
   }
 })(angular,undefined);
