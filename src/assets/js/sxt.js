7e4ae43ee72488a28ac2e3c026247cfa34f67321
shaoshun <170243414@qq.com>
2016-12-12 17:56:22 +0800

    material

--- src/assets/js/sxt.js
+++ src/assets/js/sxt.js
@@ -33,11 +33,11 @@
   win.sxt = {
     version: '1.1.12',
     app: {
-      api:'http://emp.chngalaxy.com:9091',
-      // api: 'http://10.0.9.79:7091',
-      //  api:'http://vkde.sxtsoft.com:8091',
-      fs:'http://emp.chngalaxy.com:9090'
-      // fs:'http://10.0.9.79:7090'
+      // api:'http://emp.chngalaxy.com:9091',
+      // fs:'http://emp.chngalaxy.com:9090'
+
+      api:'http://vkde.sxtsoft.com:9091',
+      fs:'http://vkde.sxtsoft.com:9090'
     },
     angular:{
 
