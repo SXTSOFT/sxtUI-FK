f5e98b7c43f8089a8c2838c6d66f314ce1c2ef8a
shaoshun <170243414@qq.com>
2016-12-14 16:02:11 +0800

    api改动

--- src/app/main/xhsc/home/applicationController.js
+++ src/app/main/xhsc/home/applicationController.js
@@ -42,28 +42,32 @@
               img: "app/main/xhsc/images/application/gxys.svg",
               state: "app.xhsc.gx.gxmain",
               stateParams: null
-            },{
-              right: vm.duties.indexOf("2")>-1,
-              title: "日常巡检",
-              type:"zl",
-              img: "app/main/xhsc/images/application/cycke.svg",
-              state: "app.xhsc.xj.main",
-              stateParams: null
-            }, {
+            },
+            // {
+            //   right: vm.duties.indexOf("2")>-1,
+            //   title: "日常巡检",
+            //   type:"zl",
+            //   img: "app/main/xhsc/images/application/cycke.svg",
+            //   state: "app.xhsc.xj.main",
+            //   stateParams: null
+            // },
+            {
               right: vm.OUType == 1 || vm.OUType == "1",
               title: "现场评估",
               type:"zl",
               img: "app/main/xhsc/images/application/xcpg.svg",
               state: "app.xhsc.download",
               stateParams: null
-            }, {
-              right:vm.duties.indexOf("2")>-1,
-              title: "移动验房",
-              type:"zl",
-              img: "app/main/xhsc/images/application/yf.svg",
-              state: "app.xhsc.yf.Main",
-              stateParams: null
-            }, {
+            },
+            // {
+            //   right:vm.duties.indexOf("2")>-1,
+            //   title: "移动验房",
+            //   type:"zl",
+            //   img: "app/main/xhsc/images/application/yf.svg",
+            //   state: "app.xhsc.yf.Main",
+            //   stateParams: null
+            // },
+            {
               right:vm.duties.indexOf("4")>-1,
               title: "材料验收",
               type:"zl",
@@ -72,47 +76,48 @@
               stateParams: null
             }
           ]
-        },{
-          type:"aq",
-          name:"安全",
-          items:[
-            {
-              right: vm.duties.indexOf("5")>-1,
-              type:"aq",
-              title: "安全验收",
-              img: "app/main/xhsc/images/application/weekys.svg",
-              state: "app.xhsc.sf.sfmain",
-              stateParams: null
-            }, {
-              right: vm.duties.indexOf("5")>-1,
-              type:"aq",
-              title: "动态危险源",
-              img: "app/main/xhsc/images/application/dyn.svg",
-              state: "app.xhsc.dyn.sfDynamicMain",
-              stateParams: null
-            },{
-              right: vm.duties.indexOf("5")>-1,
-              title: "周安全检查",
-              type:"aq",
-              img: "app/main/xhsc/images/application/weekjc.svg",
-              state: "app.xhsc.week.sfWeekMain",
-              stateParams: null
-            }
-          ]
-        },{
-          type:"jh",
-          name:"计划",
-          items:[
-            {
-              right:true,
-              title: "计划管理",
-              type:"jh",
-              img: "app/main/xhsc/images/application/plan.svg",
-              state: "app.plan.personplan",
-              stateParams: null
-            }
-          ]
-        }
+        },
+        // {
+        //   type:"aq",
+        //   name:"安全",
+        //   items:[
+        //     {
+        //       right: vm.duties.indexOf("5")>-1,
+        //       type:"aq",
+        //       title: "安全验收",
+        //       img: "app/main/xhsc/images/application/weekys.svg",
+        //       state: "app.xhsc.sf.sfmain",
+        //       stateParams: null
+        //     }, {
+        //       right: vm.duties.indexOf("5")>-1,
+        //       type:"aq",
+        //       title: "动态危险源",
+        //       img: "app/main/xhsc/images/application/dyn.svg",
+        //       state: "app.xhsc.dyn.sfDynamicMain",
+        //       stateParams: null
+        //     },{
+        //       right: vm.duties.indexOf("5")>-1,
+        //       title: "周安全检查",
+        //       type:"aq",
+        //       img: "app/main/xhsc/images/application/weekjc.svg",
+        //       state: "app.xhsc.week.sfWeekMain",
+        //       stateParams: null
+        //     }
+        //   ]
+        // },{
+        //   type:"jh",
+        //   name:"计划",
+        //   items:[
+        //     {
+        //       right:true,
+        //       title: "计划管理",
+        //       type:"jh",
+        //       img: "app/main/xhsc/images/application/plan.svg",
+        //       state: "app.plan.personplan",
+        //       stateParams: null
+        //     }
+        //   ]
+        // }
       ]
       vm.show=function (item) {
         return item.items.length&&item.items.some(function (k) {
