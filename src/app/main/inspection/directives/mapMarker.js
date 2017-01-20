50505afd292f33491cf6652638e7964b6bc2cb1a
ziuyung <jiuyuong@gmail.com>
2017-01-20 20:42:00 +0800

    ---map fix --

--- src/app/main/inspection/directives/mapMarker.js
+++ src/app/main/inspection/directives/mapMarker.js
@@ -19,16 +19,22 @@
     }
   }
   /**@ngInject*/
-  function mapMarker(mapRef){
+  function mapMarker(mapRef,$compile,$timeout){
     return {
       scope:{
         ref:'@mapMarker',
-        link:function (scope,element) {
-          mapRef.add(scope.ref,element[0]);
-          scope.$on('$destroy',function () {
-            mapRef.remove(scope.ref);
-          })
-        }
+        event:'=',
+        added:'&',
+        removed:'&'
+      },
+      link:function (scope,element) {
+        mapRef.add(scope.ref,element[0]);
+
+        if(scope.event) scope.added({marker:element[0]});
+        scope.$on('$destroy',function () {
+          mapRef.remove(scope.ref);
+          if(scope.event) scope.removed({marker:scope.event});
+        })
       }
     }
   }
