970c8ad09056d4ec168c60a3d0c685909ba8086e
lukehua <am@ricent.com>
2017-03-17 20:39:45 +0800

    材料修改

--- src/app/main/szgc/report/materialReport.controller.js
+++ src/app/main/szgc/report/materialReport.controller.js
@@ -11,6 +11,25 @@
     /** @ngInject */
     function materialReportController($scope, $filter, api, utils, $q) {
         var vm = this;
+        vm.data = [];
+        $q.all([api.szgc.vanke.projects({ page_size: 1000, page_number: 1 }),
+        api.material.MaterialService.MaterialCount()
+        ]).then(function(r){
+            r[0].data.data.forEach(function(p){
+                var c = r[1].data.Rows.find(function(count){return count.ProjectId == p.project_id});
+                if(c){
+                    p.TJ = c.TJ;
+                    p.JD = c.JD;
+                    p.ZX = c.ZX;
+                }else{
+                    p.TJ = 0;
+                    p.JD = 0;
+                    p.ZX = 0;
+                }
+                vm.data.push(p);
+            })
+        })
+        
         vm.loading = false;
     }
 })();
