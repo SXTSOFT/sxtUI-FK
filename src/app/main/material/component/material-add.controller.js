a358714d5b2aef6f0e7cf8e2417ff9d2c29feb99
lukehua <am@ricent.com>
2017-04-05 10:18:51 +0800

    材料修改

--- src/app/main/material/component/material-add.controller.js
+++ src/app/main/material/component/material-add.controller.js
@@ -13,27 +13,27 @@
     });
 
   /** @ngInject */
-  function materialAdd($scope,api, utils, $stateParams, $state) {
+  function materialAdd($scope, api, utils, $stateParams, $state) {
     var vm = this;
     vm.data = {};
     vm.data.Id = $stateParams.id;
     vm.cid = $stateParams.cid;
-    vm.pageState = vm.cid!=0 ? true : false;
+    vm.pageState = vm.cid != 0 ? true : false;
     vm.readonly = false;
 
-    api.material.type.getList({Skip: 0, Limit: 999}).then(function (g) {
+    api.material.type.getList({ Skip: 0, Limit: 999 }).then(function (g) {
       vm.materialType = g.data.Items || [];
     });
 
     if (vm.data.Id) {
       api.material.materialScience.getMaterial(vm.data.Id).then(function (r) {
         vm.data = r.data;
-        if (vm.cid != 0){
-          api.material.contract.GetContractDetailById(vm.cid,vm.data.Id).then(function (r) {
-            if(r.data){
-              vm.Brands = r.data.Brands.split('、');
-            }else {
-              vm.Brands =vm.data.Brands.split('、');
+        if (vm.cid != 0) {
+          api.material.contract.GetContractDetailById(vm.cid, vm.data.Id).then(function (r) {
+            if (r.data) {
+              vm.Brands = r.data.Brands.split(/、|,|，|；|;/);
+            } else {
+              vm.Brands = vm.data.Brands.split(/、|,|，|；|;/);
             }
           });
         }
@@ -42,12 +42,12 @@
 
     vm.save = function () {
 
-      if (vm.cid != 0){
+      if (vm.cid != 0) {
         var _str = vm.Brands.join("、");
-        var _data = {'ContractId':vm.cid,'MaterialId':vm.data.Id,'Brands':_str};
+        var _data = { 'ContractId': vm.cid, 'MaterialId': vm.data.Id, 'Brands': _str };
         api.material.contract.UpdateContract(_data).then(function () {
           utils.alert("提交成功", null, function () {
-            $state.go("app.material.materialLibrary",{cid:vm.cid});
+            $state.go("app.material.materialLibrary", { cid: vm.cid });
           });
         })
       } else {
