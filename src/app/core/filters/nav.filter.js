fe313bdd72f88cad93cd33ddc1dc6efea2e5462e
lukehua <am@ricent.com>
2017-04-07 10:13:09 +0800

    材料修改

--- src/app/core/filters/nav.filter.js
+++ src/app/core/filters/nav.filter.js
@@ -8,7 +8,7 @@
   /** @ngInject */
   function nav($stateParams, $rootScope,auth) {
     var user = auth.current();
-    console.log(user.Role.MemberType);
+    console.log(user);
     return function (items) {
       if (!user) {
         return [];
@@ -22,7 +22,6 @@
             allow = !!item.allow.find(function (allow) {
               switch (allow.user) {
                 case 'm':
-                  if (user.UserId == "13699877965") return true;
                   item.children = item.children.filter(function(c){
                     return !!c.allow.find(function (a) {
                       return (user.Role.MemberType|a.memberType) == a.memberType;
