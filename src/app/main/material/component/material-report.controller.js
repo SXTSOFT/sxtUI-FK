0455221139ef9aba958c76e6f0f27843fc72c76f
lukehua <am@ricent.com>
2017-04-21 01:28:39 +0800

    材料修改

--- src/app/main/material/component/material-report.controller.js
+++ src/app/main/material/component/material-report.controller.js
@@ -13,13 +13,22 @@
         });
 
     /** @ngInject */
-    function materialReport($scope, api, sxt, utils) {
+    function materialReport($scope, api, sxt, utils,$filter) {
         var vm = this;
+        vm.total=0;
         $scope.pageing = {
             page: 1,
-            pageSize: 10,
-            total: 0
+            pageSize: 10
         };
+        vm.rendered = false;
+        vm.date = {};
+        var dateFilter = $filter('date')
+        vm.date.eDate = new Date();
+        var date_tmp = new Date()
+        date_tmp.setDate(date_tmp.getDate() - 7);
+        vm.date.sDate = date_tmp;
+        vm.regionId = '',vm.sectionId = '';
+
         vm.host = sxt.app.api;
         vm.projects = [];
         // var mobileDetect = new MobileDetect(window.navigator.userAgent);
@@ -27,19 +36,26 @@
         // vm.isiPad = mobileDetect.mobile() == "iPad";
         var mobileDetect = new MobileDetect(window.navigator.userAgent);
         vm.isMobile = mobileDetect.mobile();
-        vm.getProjects = function () {
-            if (vm.projects.length == 0) {
-                return api.xhsc.Project.getMap().then(function (r) {
-                    vm.projects = r.data;
-                });
-            }
-        };
+        // vm.getProjects = function () {
+        //     if (vm.projects.length == 0) {
+        //         return api.xhsc.Project.getMap().then(function (r) {
+        //             vm.projects = r.data;
+        //         });
+        //     }
+        // };
 
-        vm.clearRegion = function () {
+        api.xhsc.Project.getMap().then(function (r) {
+            vm.projects = r.data;
+            vm.pids = r.data.map(function (r) { return r.ProjectID });
+            getData(vm.pids,'','','','');
+        });
+
+        vm.clearRegion = function (pid) {
             vm.regions = [];
             vm.sections = [];
-            vm.regionId = null;
-            vm.sectionId = null;
+            vm.pids = pid;
+            vm.regionId = '';
+            vm.sectionId = '';
         }
 
         vm.getAreas = function (pId) {
@@ -48,9 +64,10 @@
             });
         };
 
-        vm.clearSection = function () {
+        vm.clearSection = function (rid) {
             vm.sections = [];
-            vm.sectionId = null;
+            vm.regionId = rid;
+            vm.sectionId = '';
         }
 
         vm.getSections = function (aId) {
@@ -60,10 +77,20 @@
         };
 
         vm.getData = function (sid) {
+            vm.sectionId = sid;
+            // return api.material.materialPlan.getMaterialReport(sid, { Skip: page.Skip, Limit: page.Limit }).then(function (r) {
+            //     vm.source = r.data.Items || [];
+            //     $scope.pageing.total = r.data.TotalCount;
+            // });
+        }
+
+        function getData(pids,rid,sid,sDate,eDate){
+            vm.rendered = false;
             var page = utils.getPage($scope.pageing);
-            return api.material.materialPlan.getMaterialReport(sid, { Skip: page.Skip, Limit: page.Limit }).then(function (r) {
+            return api.material.materialPlan.getMaterialReport({pids:pids,rid:rid,sid:sid,sDate:dateFilter(sDate, 'yyyy-MM-dd'),eDate:dateFilter(eDate, 'yyyy-MM-dd'), Skip: page.Skip, Limit: page.Limit }).then(function (r) {
                 vm.source = r.data.Items || [];
-                $scope.pageing.total = r.data.TotalCount;
+                vm.total = r.data.TotalCount;
+                vm.rendered = true;
             });
         }
 
@@ -72,11 +99,16 @@
         }
 
         $scope.$watch("pageing", function () {
-            vm.getData(vm.sectionId);
+            if(vm.pids)
+                getData(vm.pids,vm.regionId,vm.sectionId,vm.date.sDate,vm.date.eDate)
         }, true);
 
         vm.printBatchCount = function () {
             $('#export').val($("#divReport").html());
+        }
+
+        vm.cx = function(){
+            getData(vm.pids,vm.regionId,vm.sectionId,vm.date.sDate,vm.date.eDate)
         }
     };
 })(angular, undefined);
