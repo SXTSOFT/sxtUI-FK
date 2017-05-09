01d368513ccfeaa3d7f4f17af40838c11f9d4f9b
lukehua <am@ricent.com>
2017-05-09 17:57:24 +0800

    材料调整

--- src/app/main/materialYs/component/materialPlanYs-checkInfo.controller.js
+++ src/app/main/materialYs/component/materialPlanYs-checkInfo.controller.js
@@ -2,18 +2,18 @@
  * Created by 陆科桦 on 2016/10/26.
  */
 
-(function (angular,undefined) {
+(function (angular, undefined) {
   'use strict';
   angular
     .module('app.xhsc')
-    .component('materialYsCheckInfo',{
-      templateUrl:'app/main/materialYs/component/materialPlanYs-checkInfo.html',
-      controller:materialPlanYsCheckInfo,
-      controllerAs:'vm'
+    .component('materialYsCheckInfo', {
+      templateUrl: 'app/main/materialYs/component/materialPlanYs-checkInfo.html',
+      controller: materialPlanYsCheckInfo,
+      controllerAs: 'vm'
     });
 
   /** @ngInject */
-  function materialPlanYsCheckInfo($rootScope,$scope,api,utils,$stateParams,xhUtils,sxt,$state,auth,$filter){
+  function materialPlanYsCheckInfo($rootScope, $scope, api, utils, $stateParams, xhUtils, sxt, $state, auth, $filter, remote) {
 
     var vm = this;
     vm.data = {};
@@ -26,65 +26,106 @@
     vm.data.Accepter = user.Name;
 
     //删除图片操作
-    $rootScope.$on('delete',function (data,index) {
+    $rootScope.$on('delete', function (data, index, id) {
+      remote.offline.delete({ Id: id });
       $scope.$apply();
     });
 
     vm.addPhoto = function (type) {
       //拍照事件
       xhUtils.photo().then(function (image) {
-        if(image){
-      // var image;
-          switch (type){
+        if (image) {
+          // var image;
+          switch (type) {
             case 1:
-              photo(type,vm.vehicleImgs,image);
+              photo(type, vm.vehicleImgs, image);
               break;
             case 2:
-              photo(type,vm.goodsImgs,image);
+              photo(type, vm.goodsImgs, image);
               break;
             case 4:
-              photo(type,vm.checkerImgs,image);
+              photo(type, vm.checkerImgs, image);
               break;
             default:
-              photo(type,vm.certificateImgs,image);
+              photo(type, vm.certificateImgs, image);
           }
           vm.data.AcceptanceTime = new Date().Format('yyyy-MM-dd hh:mm:ss');
         }
       });
     };
 
+    remote.offline.query().then(function (r) {
+      var list = r.data.filter(function (item) {
+        return item.batchId == $stateParams.id;
+      })
+      list.forEach(function (item) {
+        switch (item.type) {
+          case 1: {
+            photo2(item.Id, item.type, vm.vehicleImgs, item.img);
+            break;
+          }
+          case 2: {
+            photo2(item.Id, item.type, vm.goodsImgs, item.img);
+            break;
+          }
+          case 4: {
+            photo2(item.Id, item.type, vm.checkerImgs, item.img);
+            break;
+          }
+          default:
+            photo2(item.Id, item.type, vm.certificateImgs, item.img);
+        }
+      })
+    })
 
-    function photo(type,arr,image){
+
+    function photo(type, arr, image) {
       var _id = sxt.uuid();
-      arr.push({
+      var img = {
         Id: sxt.uuid(),
         BatchId: $stateParams.id,
-        OptionType:type,
-        ApproachStage:2,
-        ImageName:_id+".jpeg",
-        ImageUrl:_id+".jpeg",
+        OptionType: type,
+        ApproachStage: 2,
+        ImageName: _id + ".jpeg",
+        ImageUrl: _id + ".jpeg",
         ImageByte: image
-      });
+      }
+      arr.push(img);
+      remote.offline.create({ Id: img.Id, batchId: $stateParams.id, type: type, img: image });
+    }
+
+    function photo2(id, type, arr, image) {
+      var _id = sxt.uuid();
+      var img = {
+        Id: id,
+        BatchId: $stateParams.id,
+        OptionType: type,
+        ApproachStage: 2,
+        ImageName: _id + ".jpeg",
+        ImageUrl: _id + ".jpeg",
+        ImageByte: image
+      }
+      arr.push(img);
     }
 
 
     vm.data.Id = $stateParams.id;
     vm.outPutDate = new Date();
-    vm.data.AcceptanceTime = $filter('date')(new Date(),'yyyy-MM-dd hh:mm:ss');
+    vm.data.AcceptanceTime = $filter('date')(new Date(), 'yyyy-MM-dd hh:mm:ss');
 
     vm.data.WgCheck = true;
     vm.data.IsInspection = true;
 
-    $scope.$on("$destroy",function(){
+    $scope.$on("$destroy", function () {
       sendCheckResult();
       sendCheckResult = null;
     });
-    var sendCheckResult = $rootScope.$on('sendGxResult',function() {
-      if(vm.data.AcceptanceTime == null){
+    var sendCheckResult = $rootScope.$on('sendGxResult', function () {
+      if (vm.data.AcceptanceTime == null) {
         utils.alert('验收时间不能为空');
         return;
       }
-      if(vm.data.Accepter == null){
+      if (vm.data.Accepter == null) {
         utils.alert('验收人不能为空');
         return;
       }
@@ -92,7 +133,7 @@
       //   utils.alert('至少上传一张验收车辆照片');
       //   return;
       // }
-      if(vm.goodsImgs.length == 0){
+      if (vm.goodsImgs.length == 0) {
         utils.alert('至少上传一张验收货物照片');
         return;
       }
@@ -100,22 +141,30 @@
       //   utils.alert('至少上传一张验收检查人照片');
       //   return;
       // }
-      if(vm.certificateImgs.length == 0){
+      if (vm.certificateImgs.length == 0) {
         utils.alert('至少上传一张验收合格证照片');
         return;
       }
 
       vm.images = vm.vehicleImgs.concat(vm.goodsImgs).concat(vm.checkerImgs).concat(vm.certificateImgs);
       vm.data.BatchFile = vm.images;
-      if(!vm.data.WgCheck){
+      if (!vm.data.WgCheck) {
         vm.data.IsInspection = null;
       }
       api.xhsc.materialPlan.PostCheckInfo(vm.data).then(function (r) {
-        utils.alert('提交成功!',null,function () {
+        utils.alert('提交成功!', null, function () {
+          remote.offline.query().then(function (r) {
+            var list = r.data.filter(function (item) {
+              return item.batchId == vm.data.Id;
+            })
+            list.forEach(function (item) {
+              remote.offline.delete({ Id: item.Id });
+            })
+          })
           api.xhsc.materialPlan.deleteMaterialPlanBatch(vm.data.Id);
           $state.go("app.xhsc.materialys.materialdownload");
         });
       })
     });
   }
-})(angular,undefined);
+})(angular, undefined);
---
+++ src/app/main/xhsc/directive/sxtImagesMaterial.js
@@ -0,0 +1,110 @@
+/**
+ * Created by emma on 2016/5/4.
+ */
+(function(){
+  'use strict';
+  angular
+    .module('app.xhsc')
+    .directive('sxtImagesMaterial',sxtImagesDirective);
+
+  /** @ngInject */
+  function sxtImagesDirective($rootScope){
+    return {
+      restrict:'ECMA',
+      scope:{
+        sxtImagesMaterial:'='
+      },
+      link:link
+    }
+
+    function link(scope,element,attr,ctrl){
+      var player,defaultIndex,viewer,delplayer,o;
+      var imagedata = scope.sxtImagesMaterial;
+      player = function(a,e){
+        defaultIndex = $('.img img').index($(a.target))
+        if (defaultIndex == -1)
+          defaultIndex = 0;
+        var str = [];
+        str.push('<ul>')
+        angular.forEach(imagedata, function (data) {
+          var arl = data.ImageByte;
+          str.push('<li><img src="' + arl + '"></li>');
+        });
+        str.push('</ul>');
+        o = $(str.join('')).appendTo('body')
+
+        viewer = new Viewer(o[0],{
+          button:true,
+          scalable:false,
+          hide:function(){
+            viewer.destroy();
+            o.remove();
+            viewer = o=null;
+          },
+          build:function(){
+
+          },
+          view:function(){
+          }
+        });
+        viewer.show();
+        viewer.view(defaultIndex);
+        var str1 = [];
+        str1.push('<li class="viewer-delete">删除</li>');
+        $(str1.join('')).appendTo('.viewer-toolbar');
+        $('.viewer-delete').on('click',function(){
+          var nowIndex = viewer.index;
+          var id = scope.sxtImagesMaterial[nowIndex].Id;
+          scope.sxtImagesMaterial.splice(nowIndex,1);
+          viewer.hide();
+          if(imagedata.length) delplayer(nowIndex);
+          $rootScope.$broadcast('delete',nowIndex,id)
+          //console.log('de',imagedata)
+        })
+
+      }
+      delplayer = function(nowIndex){
+        // console.log('element',scope)
+
+        var str = [];
+        str.push('<ul>')
+        angular.forEach(imagedata, function (data) {
+          var arl = data.ImageByte;
+          str.push('<li><img src="' + arl + '"></li>');
+        });
+        str.push('</ul>');
+        o = $(str.join('')).appendTo('body')
+
+        viewer = new Viewer(o[0],{
+          button:true,
+          scalable:false,
+          hide:function(){
+            viewer.destroy();
+            o.remove();
+            viewer = o=null;
+          },
+          build:function(){
+
+          },
+          view:function(){
+          }
+        });
+        viewer.show();
+        viewer.view(nowIndex);
+        var str1 = [];
+        str1.push('<li class="viewer-delete">删除</li>');
+        $(str1.join('')).appendTo('.viewer-toolbar');
+        $('.viewer-delete').on('click',function(){
+          var nowIndex = viewer.index;
+          //console.log('lenght',viewer.length,nowIndex)
+          imagedata.splice(nowIndex,1);
+          viewer.hide();
+          if(imagedata.length) delplayer(nowIndex);
+          $rootScope.$emit('delete',nowIndex)
+        })
+
+      }
+      element.on ('click', player);
+    }
+  }
+})();
