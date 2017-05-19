9bca627a78b6bb29c7f3d0f67dcd3a701922b394
lukehua <am@ricent.com>
2017-05-19 15:48:31 +0800

    材料拍照

---
+++ src/app/main/xhsc/filter/fileurl.js
@@ -0,0 +1,28 @@
+/**
+ * Created by zhangzhaoyong on 16/2/15.
+ */
+(function(){
+  angular
+    .module('app.xhsc')
+    .filter('fileurl',fileurl);
+  /** @ngInject */
+  function fileurl(sxt,api){
+    return function (file,size) {
+      var value = file.Url;
+      if(!value){
+        if(!file.loading){
+          file.loading = true;
+          api.szgc.FilesService.localFile(file.Id).then(function (r) {
+            file.Url = r.data.Url;
+            file.loading = false;
+          },function () {
+            file.loading = false;
+          });
+        }
+        return '';
+      }
+      return value.indexOf('base64')!=-1?value:
+      sxt.app.api + (value && value.substring(0, 1) == '~' ? value.substring(1) : value);
+    }
+  }
+})();
